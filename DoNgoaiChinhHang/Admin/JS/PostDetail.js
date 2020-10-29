c8d4cde64cd0d0be367e40dbe6ea68e77d8fe68f
HoaHooni <62019195+HoaHooni@users.noreply.github.com>
2020-10-29 16:52:39 +0700

    con

---
+++ DoNgoaiChinhHang/Admin/JS/PostDetail.js
@@ -0,0 +1,26 @@
+﻿function BrowseServer(inputId) {
+    var finder = new CKFinder();
+    finder.BasePath = '/Plugin/ckfinder/';
+    finder.SelectFunction = SetFileField;
+    finder.SelectFunctionData = inputId;
+    finder.Popup();
+}
+
+function SetFileField(fileUrl, data) {
+    $("#ContentPlaceHolder_txtImgURL").val(fileUrl);
+    sessionStorage["ImageURL"] = fileUrl.substring(fileUrl.lastIndexOf('/') + 1, fileUrl.length);
+    $('#ContentPlaceHolder_imgProduct').attr('src', '../../Img/images/' + fileUrl.substring(fileUrl.lastIndexOf('/') + 1, fileUrl.length))
+}
+
+function SetImgControl() {
+    $("#ContentPlaceHolder_txtImgURL").val('/Admin/Img/images/' + sessionStorage["ImageURL"]);
+    $('#ContentPlaceHolder_imgProduct').attr('src', '../../Img/images/' + sessionStorage["ImageURL"])
+}
+
+$(() => {
+    var isReloadImg = sessionStorage["ReloadImg"];
+    if (isReloadImg != null && isReloadImg == "true") {
+        SetImgControl();
+    }
+
+})
---
+++ DoNgoaiChinhHang/Admin/JS/ProductDetail.js
@@ -0,0 +1,14 @@
+﻿
+function BrowseServer(inputId) {
+    var finder = new CKFinder();
+    finder.BasePath = '/Plugin/ckfinder/';
+    finder.SelectFunction = SetFileField;
+    finder.SelectFunctionData = inputId;
+    finder.Popup();
+}
+
+function SetFileField(fileUrl, data) {
+    $("#ContentPlaceHolder_txtImgURL").val(fileUrl);
+    sessionStorage["ImageURL"] = fileUrl.substring(fileUrl.lastIndexOf('/') + 1, fileUrl.length);
+    $('#ContentPlaceHolder_imgProduct').attr('src', '../../Img/images/' + fileUrl.substring(fileUrl.lastIndexOf('/') + 1, fileUrl.length))
+}
